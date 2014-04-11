0e13f373888fb25dadbe500d0816b40ad659f8dc
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-04-11 12:06:26 +0200

    mps.core.fertigung.IFertigung systemoperation fertigungsAuftragErstellen zu fertigungsPlanErstellen umbenannt, weitere dokumentation dort geschrieben

--- src/mps/core/fertigung/IFertigung.java
+++ src/mps/core/fertigung/IFertigung.java
@@ -7,11 +7,18 @@ public interface IFertigung {
 	/**Systemoperation*/
 	
 	/** Erstellt fuer eine gegebene auftrags-ID einen Fertigungsplan
-	 *  beziehungsweise Fertigungsauftrag und gibt dessen ID zurueck.
+	 *  und gibt die ID des entsprechenden Fertigungsauftrags zurueck.
+	 *  Der Fertigungsplan ist Textdatei, in der die einzelnen Bauteile
+	 *  und dazugehörigen Arbeitsplaene samt Vorgaengen stukturiert 
+	 *  dargestellt sind, womit die Fertigung ihre Fertigungsauftraege 
+	 *  durchfuehren koennen 
 	 * @param auftragNr ID des auftrags 
 	 * @return ID des Fertigungsplans/Fertigungsauftrags
+	 * Nicht spezifiziert: Format der Textdatei 
+	 * Alternativ wird hier nur eine textuelle Form der im Datenmodell 
+	 * der Komponente "Fertigung" hinterlegte Daten ausgegeben.  
 	 */
-	long fertigungsAuftragErstellen(long auftragNr);
+	long fertigungsPlanErstellen(long auftragNr);
 	
 	/**Weitere Operationen*/
 	//void fertige(long fertigungsAuftragId); //soll durch was sinnvolleres ersetzt werden oder komplett gestrichen werden
