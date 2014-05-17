9d2d1d38b6911ab123adc68dbca71dbf7df5b4e8
krystian <krystian@192.168.1.20>
2014-05-17 15:36:23 +0200

    changed ArrayList serverList to a HashMap with the Servernames as Keys and the Server Objects as values. This way we could find the right Server Object to delete without having to call getName() on it first. This way we prevent a RemoteException when we try to terminate one of the servers, because we don't try to call methods on a terminated server anymore.

--- src/mps/redundant/Dispatcher.java
+++ src/mps/redundant/Dispatcher.java
@@ -10,149 +10,163 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 
+public class Dispatcher implements IDispatcher {
+	public static Registry serverRegistry;
+	public HashMap<String, IMpsServer> serverList;
+	public int roundRobinCounter = 0;
+	public static Dispatcher dispatcher = null;
 
+	public Dispatcher() throws RemoteException {
+		dispatcher = this;
+		this.serverRegistry = LocateRegistry
+				.createRegistry(Config.REGISTRY_PORT);
+		this.serverList = new HashMap<String, IMpsServer>();
+	}
 
-public class Dispatcher implements IDispatcher{
-    public static Registry serverRegistry;
-    public ArrayList<IMpsServer> serverList;
-    public int roundRobinCounter = 0;
-    public static Dispatcher dispatcher = null;
+	public static Dispatcher create() throws RemoteException {
+		if (dispatcher == null) {
+			dispatcher = new Dispatcher();
+			IDispatcher stub = (IDispatcher) UnicastRemoteObject.exportObject(
+					dispatcher, 0);
+			Registry dispatcherRegistry = dispatcher.serverRegistry;
+			dispatcherRegistry.rebind(Config.DISPATCHER_NAME, stub); // treagt
+																		// den
+																		// dispatcher
+																		// unter
+																		// dem
+																		// namen
+																		// in
+																		// die
+																		// Registry
+																		// ein
+			System.out.println("Namen in Registry:"
+					+ Arrays.toString(serverRegistry.list()));
+		}
+		return dispatcher;
+	}
 
-    public Dispatcher() throws RemoteException {
-        dispatcher = this;
-        this.serverRegistry = LocateRegistry.createRegistry(Config.REGISTRY_PORT);
-        this.serverList = new ArrayList<IMpsServer>();
-    }
+	@Override
+	public IMpsServer getRemoteServerInstance() throws RemoteException {
+		try {
+			System.out.println("Namen in Registry:"
+					+ Arrays.toString(serverRegistry.list()));
+		} catch (RemoteException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
 
-    public static Dispatcher create() throws RemoteException {
-        if(dispatcher == null){
-            dispatcher = new Dispatcher();
-            IDispatcher stub = (IDispatcher) UnicastRemoteObject.exportObject(dispatcher, 0);
-            Registry dispatcherRegistry = dispatcher.serverRegistry;
-            dispatcherRegistry.rebind(Config.DISPATCHER_NAME, stub); //treagt den dispatcher unter dem namen in die Registry ein
-            System.out.println("Namen in Registry:"+ Arrays.toString(serverRegistry.list()));
-        }
-        return dispatcher;
-    }
+		return getNextRemoteObject();
+	}
 
-    @Override
-    public IMpsServer getRemoteServerInstance() throws RemoteException{
-        try {
-            System.out.println("Namen in Registry:"+ Arrays.toString(serverRegistry.list()));
-        } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+	private synchronized IMpsServer getNextRemoteObject() {
+		IMpsServer server = null;
+		try {
+			server = getNextActiveServer();
+			System.out.println("Server: " + server.getName()
+					+ " uebernimmt die Anfrage.");
+			while (server == null) {
+				Thread.sleep(5000);
+				server = getNextActiveServer();
+			}
+			System.out.println(server.getName());
+		} catch (RemoteException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		} catch (InterruptedException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+		return server;
+	}
 
-        return getNextRemoteObject();
-    }
+	private IMpsServer getNextActiveServer() throws RemoteException {
+		roundRobinCounter %= serverList.size();
+		IMpsServer server;
+		while (roundRobinCounter < serverList.size()) {
+			ArrayList<IMpsServer> servers = new ArrayList<IMpsServer>(serverList.values());
+			server = servers.get(roundRobinCounter);
+			if (!server.isDeaktiviert()) {
+				// TODO dirty aber geht erstmal nicht besser
+				if (server.getName().equals(Config.HAWMPS1_NAME)) {
+					MonitorGUI.getInstance().erhoeheAnfragenVonMPS1();
+				}
+				if (server.getName().equals(Config.HAWMPS2_NAME)) {
+					MonitorGUI.getInstance().erhoeheAnfragenVonMPS2();
+				}
+				roundRobinCounter++;
+				return server;
+			}
+			roundRobinCounter++;
+			if (roundRobinCounter == serverList.size())
+				roundRobinCounter = 0;
+		}
+		return null; // To change body of created methods use File | Settings |
+						// File Templates.
+	}
 
-    private synchronized IMpsServer getNextRemoteObject(){
-        IMpsServer server = null;
-        try {
-            server = getNextActiveServer();
-            System.out.println("Server: "+ server.getName() + " uebernimmt die Anfrage.");
-            while (server == null) {
-                Thread.sleep(5000);
-                server = getNextActiveServer();
-            }
-            System.out.println(server.getName());
-        } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        return server;
-    }
+	public synchronized void alive(IMpsServer serverInstance)
+			throws RemoteException {
+		if (!serverList.values().contains(serverInstance)) {
+			serverList.put(serverInstance.getName(), serverInstance);
+		}
+	}
 
-    private IMpsServer getNextActiveServer() throws RemoteException {
-       roundRobinCounter %= serverList.size();
-       IMpsServer server;
-       while(roundRobinCounter < serverList.size()) {
-            server = serverList.get(roundRobinCounter);
-            if (!server.isDeaktiviert()) {
-                //TODO dirty aber geht erstmal nicht besser
-                if(server.getName().equals(Config.HAWMPS1_NAME)){MonitorGUI.getInstance().erhoeheAnfragenVonMPS1();}
-                if(server.getName().equals(Config.HAWMPS2_NAME)){MonitorGUI.getInstance().erhoeheAnfragenVonMPS2();}
-                roundRobinCounter++;
-                return server;
-            }
-           roundRobinCounter++;
-           if(roundRobinCounter == serverList.size()) roundRobinCounter = 0;
-        }
-        return null;  //To change body of created methods use File | Settings | File Templates.
-    }
+	public synchronized void notAlive(String serverName) {
+		if (serverList.containsKey(serverName)) {
+			serverList.remove(serverName);
+		}
+	}
 
-    public synchronized void alive(IMpsServer serverInstance){
-        if(!serverList.contains(serverInstance)){
-            serverList.add(serverInstance);
-        }
-    }
+	public void deaktiviereServerInstanz(String servername, boolean b) {
+		try {
+			if (serverList.containsKey(servername)) {
+				serverList.get(servername).setisDeaktiviert(b);
+			}
 
-    public synchronized void notAlive(IMpsServer serverInstance){
-        if(serverList.contains(serverInstance)){
-            serverList.remove(serverInstance);
-        }
-    }
+		} catch (RemoteException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+	}
 
-    public void deaktiviereServerInstanz(String servername,boolean b) {
-        for (IMpsServer server : serverList) {
-            try{
-                if (server.getName().equals(servername)) {
-                        server.setisDeaktiviert(b);
+	public boolean isServerOnline(String servername) {
 
-                }
-            } catch (RemoteException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
-    }
+				if (serverList.containsKey(servername)) {
+					return true;
+		}
+		return false;
+	}
 
-    public boolean isServerOnline(String servername) {
-        for (IMpsServer iMpsServer : serverList) {
-            try {
-                if (servername.equals(iMpsServer.getName()))
-                    return true;
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-        return false;
-    }
-   
+	public static void main(String[] args) throws RemoteException,
+			InterruptedException, NotBoundException {
+		// if (System.getSecurityManager() == null) {
+		// System.setSecurityManager(new SecurityManager());
+		// }
+		// try {
+		if (args.length == 2) {
+			Config.REGISTRY_HOST = args[0];
+			Config.REGISTRY_PORT = Integer.parseInt(args[1]);
+			JFrame frame = new JFrame("MonitorGUI");
+			MonitorGUI x = new MonitorGUI(frame);
+			frame.setContentPane(x.monitorGUI);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(false);
+			frame.setVisible(true);
 
+			Dispatcher dispatcher = Dispatcher.create();
+			Monitor.create(dispatcher);
+		} else
+			System.err
+					.println("please specify your ip adress and port as parameters");
 
-    
-    public static void main(String[] args) throws RemoteException, InterruptedException, NotBoundException {
-//    	   if (System.getSecurityManager() == null) {
-//               System.setSecurityManager(new SecurityManager());
-//           }
-//           try {
-    	if(args.length == 2){
-    		Config.REGISTRY_HOST = args[0];
-    		Config.REGISTRY_PORT = Integer.parseInt(args[1]);
-            JFrame frame = new JFrame("MonitorGUI");
-            MonitorGUI x = new MonitorGUI(frame);
-            frame.setContentPane(x.monitorGUI);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setResizable(false);
-            frame.setVisible(true);
+		// } catch (Exception e) {
+		// System.err.println("Dispatcher exception:");
+		// e.printStackTrace();
+		// }
+	}
 
-            Dispatcher dispatcher = Dispatcher.create();
-            Monitor.create(dispatcher);
-    	}
-    	else System.err.println("please specify your ip adress and port as parameters");
-
-
-
-//           } catch (Exception e) {
-//               System.err.println("Dispatcher exception:");
-//               e.printStackTrace();
-//           }
-    }
-    
-    
-    
 }
--- src/mps/redundant/Monitor.java
+++ src/mps/redundant/Monitor.java
@@ -13,106 +13,106 @@ import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
 public class Monitor implements IMonitor {
-    public Dispatcher dispatcher;
-    public HashMap<String, Timer> aliveTimer;
-    public Registry hawmps1reg;
-    public Registry hawmps2reg;
+	public Dispatcher dispatcher;
+	public HashMap<String, Timer> aliveTimer;
+	public Registry hawmps1reg;
+	public Registry hawmps2reg;
 
-    private Monitor(Dispatcher dispatcher) {
-        this.dispatcher = dispatcher;
-        this.aliveTimer = new HashMap<String, Timer>();
-    }
+	private Monitor(Dispatcher dispatcher) {
+		this.dispatcher = dispatcher;
+		this.aliveTimer = new HashMap<String, Timer>();
+	}
 
-    public static Monitor create(Dispatcher dispatcher) throws RemoteException {
-        Monitor monitor = new Monitor(dispatcher);
-        IMonitor stub = (IMonitor) UnicastRemoteObject.exportObject(monitor, 0);
+	public static Monitor create(Dispatcher dispatcher) throws RemoteException {
+		Monitor monitor = new Monitor(dispatcher);
+		IMonitor stub = (IMonitor) UnicastRemoteObject.exportObject(monitor, 0);
 
-        Registry heartBeatRegistry = dispatcher.serverRegistry;
-        heartBeatRegistry.rebind(Config.MONITOR_NAME, stub); //treagt den Monitor unter dem namen in die Registry ein
-        System.out.println("Namen in Registry:"+ Arrays.toString(dispatcher.serverRegistry.list()));
-        return monitor;
-    }
-    
-    public void getMpsServerRegistry(String serverName, String host, int port) throws RemoteException{
-    	if(serverName.equals("hawmps1")){
-    	hawmps1reg = LocateRegistry.getRegistry(host, port);}
-    	else{
-    	hawmps2reg = LocateRegistry.getRegistry(host, port);}
-    }
+		Registry heartBeatRegistry = dispatcher.serverRegistry;
+		heartBeatRegistry.rebind(Config.MONITOR_NAME, stub); // treagt den
+																// Monitor unter
+																// dem namen in
+																// die Registry
+																// ein
+		System.out.println("Namen in Registry:"
+				+ Arrays.toString(dispatcher.serverRegistry.list()));
+		return monitor;
+	}
 
-    @Override
-    public void alive(String serverName) {
-        System.out.println("HeartBeat empfangen: " + serverName);
-        if (aliveTimer.containsKey(serverName)) {
-            aliveTimer.get(serverName).cancel();
-            aliveTimer.remove(serverName);
-        }
-        aliveTimer.put(serverName, startTimer(serverName));
-        changeZustand(serverName, MonitorGUI.Zustand.online);
+	public void getMpsServerRegistry(String serverName, String host, int port)
+			throws RemoteException {
+		if (serverName.equals("hawmps1")) {
+			hawmps1reg = LocateRegistry.getRegistry(host, port);
+		} else {
+			hawmps2reg = LocateRegistry.getRegistry(host, port);
+		}
+	}
 
+	@Override
+	public void alive(String serverName) {
+		System.out.println("HeartBeat empfangen: " + serverName);
+		if (aliveTimer.containsKey(serverName)) {
+			aliveTimer.get(serverName).cancel();
+			aliveTimer.remove(serverName);
+		}
+		aliveTimer.put(serverName, startTimer(serverName));
+		changeZustand(serverName, MonitorGUI.Zustand.online);
 
-        try {
-        	if(serverName.equals("hawmps1")){
-            dispatcher.alive((IMpsServer) hawmps1reg.lookup(serverName));}
-        	else{
-        		dispatcher.alive((IMpsServer) hawmps2reg.lookup(serverName));
-        	}
-        } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (NotBoundException e) {
-            //e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
+		try {
+			if (serverName.equals("hawmps1")) {
+				dispatcher.alive((IMpsServer) hawmps1reg.lookup(serverName));
+			} else {
+				dispatcher.alive((IMpsServer) hawmps2reg.lookup(serverName));
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		} catch (NotBoundException e) {
+			// e.printStackTrace(); //To change body of catch statement use File
+			// | Settings | File Templates.
+		}
+	}
 
-    public void notAlive(String serverName) {
-        aliveTimer.remove(serverName);
-        try {
-            IMpsServer server = (IMpsServer) dispatcher.serverRegistry.lookup(serverName);
-            dispatcher.notAlive(server);
-            changeZustand(serverName, MonitorGUI.Zustand.offline);
-        } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (NotBoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
+	public void notAlive(String serverName) {
+		dispatcher.notAlive(serverName);
+		aliveTimer.remove(serverName);
+			changeZustand(serverName, MonitorGUI.Zustand.offline);
+	}
 
-    private Timer startTimer(final String serverName) {
-        final Monitor monitor = this;
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                monitor.notAlive(serverName);
-            }
-        }, Config.MONITOR_SERVERTIMEOUT);
-        return timer;
-    }
+	private Timer startTimer(final String serverName) {
+		final Monitor monitor = this;
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				monitor.notAlive(serverName);
+			}
+		}, Config.MONITOR_SERVERTIMEOUT);
+		return timer;
+	}
 
-    private void changeZustand(String serverName, MonitorGUI.Zustand zustand) {
-        IMpsServer derServer = null;
-        try {
-            for (IMpsServer server : dispatcher.serverList) {
-                    if (server.getName().equals(serverName)) {
-                        derServer = server;
-                    }
-            }
+	private void changeZustand(String serverName, MonitorGUI.Zustand zustand) {
+		IMpsServer derServer = null;
+		try {
+			if (dispatcher.serverList.containsKey(serverName)) {
+					derServer = dispatcher.serverList.get(serverName);
+				}
 
-            if ((derServer != null && !derServer.isDeaktiviert()) || zustand.equals(MonitorGUI.Zustand.offline)) {
-                if (serverName.equals(Config.HAWMPS1_NAME)) {
-                        MonitorGUI.getInstance().changeZustandMPS1(zustand);
-                } else if (serverName.equals(Config.HAWMPS2_NAME)) {
-                        MonitorGUI.getInstance().changeZustandMPS2(zustand);
-                } else {
-                    throw new UnknownError("Unbekannter Servername: " + serverName);
-                }
-            }
+			if ((derServer != null && !derServer.isDeaktiviert())
+					|| zustand.equals(MonitorGUI.Zustand.offline)) {
+				if (serverName.equals(Config.HAWMPS1_NAME)) {
+					MonitorGUI.getInstance().changeZustandMPS1(zustand);
+				} else if (serverName.equals(Config.HAWMPS2_NAME)) {
+					MonitorGUI.getInstance().changeZustandMPS2(zustand);
+				} else {
+					throw new UnknownError("Unbekannter Servername: "
+							+ serverName);
+				}
+			}
 
-
-        } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
+		} catch (RemoteException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+	}
 }
